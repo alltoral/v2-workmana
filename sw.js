99aa03e4a89aeea9558e2dcb844a9c4ccf5becde
alltoral <modo.alltoral@gmail.com>
2026-09-18 08:39:17 -0300

    Update CACHE_NAME to version 21

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Only caches the static app shell (HTML/CSS/JS/icons) so the interface can
 // open offline. Firestore data still needs an internet connection to sync.
 
-var CACHE_NAME = "workmana-shell-v19";
+var CACHE_NAME = "workmana-shell-v21";
 var APP_SHELL = [
   "./",
   "./index.html",
